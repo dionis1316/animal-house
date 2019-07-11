0a626fe93518b568cddef7c10f4b3a8e07566311
Dionis Campines <info@dcsolution.net>
2019-07-10 22:09:29 -0500

    mi primer commit

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String
+    },
+
+    last_name: {
+        type: String
+    },
+    age: {
+        type: String
+        
+    },
+    address: {
+        type: String
+    }, 
+
+    phone : {
+        type: String
+
+    },
+
+    email: {
+        type : String
+        
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
---
+++ routes/Users.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const users = express.Router()
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+const User = require("../models/User")
+users.use(cors())
+
+process.env.SECRET_KEY =  'secret'
+
+users.post('/registrer', (req,res) => {
+    const today = new Date()
+    const userData = {
+        first_name: req.body.first_name,
+        last_name: req.boby.last_name,
+        age: req.boby.age,
+        address: req.body.address,
+        phone: req.body.phone,
+        email: req.boby.email,
+        password: req.boby.password,
+        create: today
+
+    }
+
+    User.findOne({
+        email: req.boby.email
+    })
+    .then( user => {
+        if(!user) {
+            bcrypt.hash(req.boby.password, 10, (err, hash) => {
+                userData.password = hash
+                User.create(userData)
+                .then (user => {
+                    res.json({ status: user.email + ' Registrado!' })
+                })
+                .catch(err => {
+                    res.send('error: '+ err)
+                })
+            })
+        }else{
+            res.json({error: 'El Usuario ya Existe'})
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+})
+
+module.exports = users
